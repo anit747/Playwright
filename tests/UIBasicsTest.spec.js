822875a841ada5f45258c0d4c1b7ae52847b6695
anitkumarrai <anit.rai@chamberlain.com>
2024-10-19 19:48:46 +0530

    Last Commit

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,62 +1,121 @@
-const {test,expect}= require('@playwright/test');
+ const {test, expect} = require('@playwright/test');
 
 
-test('Browser Context Playwright Test', async  ({browser})=>{
 
-const context = await browser.newContext();
-const page = await context.newPage();
-await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-const username = page.locator('input#username');
-await username.fill("anitrai747@gmail.com");
-await page.locator('input#password').fill("learning");
-await page.locator("[type='submit']").click(); 
-let extractMess = await page.locator("[style*='display: block;']").textContent();
-console.log(extractMess);
- await expect(page.locator("[style*='display: block;']")).toContainText('Incorrect'); 
+ //test.use({ browserName: 'webkit'});
+ test('@Web Browser Context-Validating Error login', async ({browser})=>
+ {
+   
+      const context = await browser.newContext();
+      const page =  await context.newPage();
+    // page.route('**/*.{jpg,png,jpeg}',route=> route.abort());
+      const userName = page.locator('#username');
+      const signIn = page.locator("#signInBtn");
+      const cardTitles =  page.locator(".card-body a");
+      page.on('request',request=> console.log(request.url()));
+      page.on('response',response=> console.log(response.url(), response.status()));
+      await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+      console.log(await page.title());
+      //css 
+     await userName.fill("rahulshetty");
+     await page.locator("[type='password']").fill("learning");
+     await signIn.click();
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+    //type - fill
+    await userName.fill("");
+    await userName.fill("rahulshettyacademy");
+    await signIn.click();
+    console.log(await cardTitles.first().textContent());
+   console.log(await cardTitles.nth(1).textContent());
+   const allTitles = await cardTitles.allTextContents();
+   
+   console.log(allTitles);
 
-});
-
-test('UI Controls',async ({page})=>
-    {
-
- 
-        await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-        const username = page.locator('input#username');
-        const documenLink = page.locator("[href*='documents-request']")
-        await username.fill("anitrai747@gmail.com");
-        await page.locator('input#password').fill("learning");
-
-        const dropdown = page.locator("select.form-control");
-        await dropdown.selectOption("consult");
-        await page.locator(".radiotextsty").last().click();
-        await page.locator("#okayBtn").click();
-        await expect(page.locator(".radiotextsty").last()).toBeChecked();
-        await page.locator("#terms").click();
-        await expect(page.locator("#terms")).toBeChecked();
-        await page.locator("#terms").uncheck();
-        expect (await page.locator("#terms").isChecked()).toBeFalsy();
-        await expect (documentLink).toHaveAttribute("class","blinkingText")
+ });
  
 
-
-});
-
-
-
-test.only('Child Window Handle',async ({browser})=>
-{
-    const context = await browser.newContext();
-    const page = await context.newPage();
-
+ test('@Web UI Controls', async ({page})=>
+ {
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-    const documenLink = page.locator("[href*='documents-request']");
+    const userName = page.locator('#username');
+    const signIn = page.locator("#signInBtn");
+    const documentLink = page.locator("[href*='documents-request']");
+    const dropdown = page.locator("select.form-control");
+    await dropdown.selectOption("consult");
+    await page.locator(".radiotextsty").last().click();
+    await page.locator("#okayBtn").click();
+    console.log(await page.locator(".radiotextsty").last().isChecked());
+    await expect(page.locator(".radiotextsty").last()).toBeChecked();
+    await page.locator("#terms").click();
+    await expect( page.locator("#terms")).toBeChecked();
+    await page.locator("#terms").uncheck();
+    expect( await page.locator("#terms").isChecked()).toBeFalsy();
+    await expect(documentLink).toHaveAttribute("class","blinkingText");
+ });
 
-    const [newPage]= Promise.all(
-    [context.waitForEvent('page') ,//listen for any new page . promise: penidng , rejected, fullfilled 
+ 
 
-    documenLink.click(),
-]);
+ test('@Child windows hadl', async ({browser})=>
+ {
+    const context = await browser.newContext();
+    const page =  await context.newPage();
+    const userName = page.locator('#username');
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink = page.locator("[href*='documents-request']");
 
-const  text  =await  newPage.locator(".red").textContent();
-console.log(text);
-})
+    const [newPage]=Promise.all(
+   [
+      context.waitForEvent('page'),//listen for any new page pending,rejected,fulfilled
+      documentLink.click(),
+   
+   ])//new page is opened
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+    
+
+
+
+
+
+   const  text = await newPage.locator(".red").textContent();
+    const arrayText = text.split("@")
+    const domain =  arrayText[1].split(" ")[0]
+    console.log(domain);
+    await page.locator("#username").fill(domain);
+    console.log(await page.locator("#username").textContent());
+
+
+
+
+    
+    
+
+
+
+
+
+ })
+
+
+
+ 
+
+
+
--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -1,103 +1,46 @@
-const {test,expect,request}= require('@playwright/test');
-const loginPayload = {userEmail: "anitrai747@gmail.com", userPassword: "Iamanitrai@747"};
-let token;
-test.beforeAll(async()=>
-  {
+const {test, expect, request} = require('@playwright/test');
+const {APiUtils} = require('../utils/APiUtils');
+const loginPayLoad = {userEmail:"anshika@gmail.com",userPassword:"Iamking@000"};
+const orderPayLoad = {orders:[{country:"Cuba",productOrderedId:"6262e95ae26b7e1a10e89bf0"}]};
 
 
-const apiContext = await request.newContext();
-const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
-    {
-        data: loginPayload
-    }) //200,201
+let response;
+test.beforeAll( async()=>
+{
+   const apiContext = await request.newContext();
+   const apiUtils = new APiUtils(apiContext,loginPayLoad);
+   response =  await apiUtils.createOrder(orderPayLoad);
+
+})
 
 
-expect( loginResponse.ok() ).toBeTruthy();
-const loginResponseJson = await loginResponse.json();
-  token = loginResponseJson.token;
-console.log(token);
+//create order is success
+test('@API Place the order', async ({page})=>
+{ 
+    page.addInitScript(value => {
 
-});
+        window.localStorage.setItem('token',value);
+    }, response.token );
+await page.goto("https://rahulshettyacademy.com/client");
+ await page.locator("button[routerlink*='myorders']").click();
+ await page.locator("tbody").waitFor();
+const rows = await page.locator("tbody tr");
 
 
-test.beforeEach(()=>{
-
-
-});
-
-test('Client App Login', async  ({page})=>
-    {
-     
-     page.addInitScript(value => {
-
-      window.localStorage.setItem("token", value);
-     }, token);
-     const email = "";
-     const productName = "ZARA COAT 3";
-    await page.goto("https://rahulshettyacademy.com/client/");
-    const products = page.locator(".card-body");
-
-     
-    let extractMess = await page.locator(".card-body b").allTextContents();
-    console.log(extractMess);
-    const count = await products.count(); 
-    
-    for(let i =0;i<count;i++){
-    if(await products.nth(i).locator("b").textContent()=== productName)
-        {
-            //add to cart
-            
-           await products.nth(i).locator("text= Add To Cart").click();
-             break;
-    
-    }
-    
-    }
-    
-    await page.locator("[routerlink*='cart']").click(); 
-    await page.locator("div li").first().waitFor();
-    const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
-    expect(bool).toBeTruthy(); 
-    
-    await page.locator("text=Checkout").click();
-    await page.locator("[placeholder*='Country']").pressSequentially("ind"); //to type sequentially 
-    const dropdown = page.locator(".ta-results");
-    await dropdown.waitFor();
-    const optionsCount = await dropdown.locator("button").count();
-    for(let i =0;i<optionsCount;i++)
-    {
-      const text = await dropdown.locator("button").nth(i).textContent();
-      if(text==" India")
-        {
-            await dropdown.locator("button").nth(i).click();
-            break; 
-    
-      }
-    
-    }
-    // await page.locator(".field [type = 'text']").fill("4542 9931 9292 2293");
-    await page.locator("[class='field small'] [class='input txt']").fill("456");
-    await page.locator("[class='field'] [class='input txt']").fill("Anit");
-    await expect(page.locator(".user__name [type='text']").first()).toHaveText("anitrai747@gmail.com");
-    await page.locator(".action__submit").click();
-    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
-    const orderID = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
-    console.log(orderID);
-    
-    await page.locator("button[routerlink ='/dashboard/myorders']").first().click();
-    await page.locator("tbody").waitFor();
-    const OrdersCount = await page.locator("tbody tr");
-    
-    for(let i =0;i<await OrdersCount.count();i++){
-    
-       const rowOrderid = await  OrdersCount.nth(i).locator("th").textContent();
-       if(orderID.includes(rowOrderid)){
-       await OrdersCount.nth(i).locator("button").first().click();
+for(let i =0; i<await rows.count(); ++i)
+{
+   const rowOrderId =await rows.nth(i).locator("th").textContent();
+   if (response.orderId.includes(rowOrderId))
+   {
+       await rows.nth(i).locator("button").first().click();
        break;
-    
-       }
-    }
-    
-    const orderIdDetails = await page.locator(".col-text").textContent();
-    expect(orderID.includes(orderIdDetails)).toBeTruthy();
-     });
+   }
+}
+const orderIdDetails =await page.locator(".col-text").textContent();
+//await page.pause();
+expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
+
+});
+
+//Verify if order created is showing in history page
+// Precondition - create order -
